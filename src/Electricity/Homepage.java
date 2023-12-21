1bab33aee3df64e4ad9c77f51136429b381aa3db
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2023-12-21 12:24:37 +0530

    Created Frame for Homepage

---
+++ src/Electricity/Homepage.java
@@ -0,0 +1,43 @@
+package Electricity;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Homepage  extends JFrame{
+	
+	Homepage()
+	{
+		//to get image in frame
+		ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg"));
+		//to adjust image size into frame
+		Image i2= i1.getImage().getScaledInstance(730, 550, Image.SCALE_DEFAULT);
+		ImageIcon i3= new ImageIcon(i2);
+		//we cannot add image directly into add(); funtion so we use JLabel Class
+		 
+		JLabel image= new JLabel(i3);
+		add(image);
+		
+		setVisible(true);
+		
+		for(int i=1;i<500;i++)
+		{
+			setSize(i,i);
+			setLocation(400,150);
+			
+		}
+		
+		
+	
+		
+	}
+
+	public static void main(String[] args) {
+	
+		new Homepage();
+
+	}
+
+}
